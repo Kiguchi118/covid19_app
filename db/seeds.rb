c9a7c13e3a3eb019c0dd18f70848a65fdd9de7b3
KIguchi <kiguchi118@gmail.com>
2020-05-14 04:52:16 +0900

    Add clinic data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require "csv"
+
+CSV.foreach('db//csv/clinic.csv', headers: true) do |row|
+  Clinic.create(
+    name: row['name'],
+    city: row['city']
+  )
+end
